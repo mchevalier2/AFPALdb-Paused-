ffd50b2339cc14a978373ed102fbf237b761d568
mchevalier2 <chevalier.manuel@gmail.com>
2016-02-05 14:25:02 +0100

    test

---
+++ RAFPALdb/R/RAFPALdb.R
@@ -0,0 +1,26 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+test_fct=function(){
+    library(RMySQL)
+    library(rjson)
+    
+    con=dbConnect(MySQL(), "AFPALdb","Manuu","cgkrggjn")
+    
+    
+    rs=dbSendQuery(con, "select * from dataset where Dataset_ID=1")
+    d1=dbFetch(rs)
+    fromJSON(d1[6][1,])
+    
+    
+    
+    rs=dbSendQuery(con, "select * from dataset where Dataset_ID=2")
+    d1=dbFetch(rs)
+    fromJSON(d1[6][1,])
+}
