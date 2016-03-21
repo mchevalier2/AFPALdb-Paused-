479878e1e1e04594400f8c07f6dab5573574a701
mchevalier2 <chevalier.manuel@gmail.com>
2016-03-21 18:48:59 +0100

    Monte Carlo quasi

---
+++ RAFPALdb/R/mov.avg.R
@@ -0,0 +1,9 @@
+#' Performs a moving average. 
+#'
+#' This function performs a mpoving average.
+#' @keywords unpackJSON
+#' @export
+#' @examples
+#' .mov.avg(1:100)
+
+.mov.avg=function(x,n=11){filter(x,rep(1/n,n), sides=2)}
--- RAFPALdb/R/unpackJSON.R
+++ RAFPALdb/R/unpackJSON.R
@@ -1,4 +1,5 @@
-#' 
+
+#' #' 
 #'
 #' This function sends a request to the database and tests the result.
 #' @keywords unpackJSON
@@ -11,7 +12,15 @@
 {
     if(is.na(json)) return(NULL)
     dat=rjson::fromJSON(json)
-    if(length(dat)==1) return(as.vector(unlist(dat[[1]])))
+    if(length(dat)==1) return(as.numeric(as.vector(unlist(dat[[1]]))))
+    dat=lapply(dat,function(x) return(lapply(x,function(y) return(ifelse(is.null(y),NA,y)))))
     dat=t(do.call(rbind.data.frame, dat)) ; rownames(dat)=1:nrow(dat)
-    return(dat)
+    cc=colnames(dat)
+    if(sum(sapply(cc,function(x) return(is.na(as.numeric(x)))))==0){ # If there is no conversion of a string to a float
+        dat=dat[,order(as.numeric(cc))]
+    }
+    dat2=matrix(0,ncol=ncol(dat),nrow=nrow(dat))
+    for(i in 1:ncol(dat)) dat2[,i]=as.vector(as.numeric(dat[,i]))
+    colnames(dat2)=colnames(dat)
+    return(dat2)
 }
